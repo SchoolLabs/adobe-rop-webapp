67d0db23561d86b0ae32b9101944ffa9d88be855
Sean Helvey <sean.helvey@gmail.com>
2015-02-14 12:44:26 -0700

    testing fake grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -190,25 +190,26 @@ function animate(img, delta, last){
   function build_grid(){
       $("#main-img").hide();
       $("#grid").css({"opacity":1});
-        var scores = sessionStorage.getItem("scores").split(",");
-        var sum = 0;
-        for (var i = 0; i < scores.length; i++){
-            sum += parseInt(scores[i]);
-            if (i % 5 == 0) {
-                $("#grid").append( "<div class='row grid-row'></div>" );
-            }
-            if (scores[i] == 1) {
-                $(".grid-row:last").append( "<div class='col-xs-15 grid-img'><img class='img' src='assets/img/Grid"+ (i + 1) +".png'></div>" );
-            } else {
-                $(".grid-row:last").append( "<div class='col-xs-15 grid-img incorrect'><img class='img' src='assets/img/Grid"+ (i + 1) +".png'></div>" );
-            }
-        }
-        $(".img").load(function(){
-            $(".incorrect").addClass("cross");
-            // $("#footer").height(total - header - img_text_row - $("#grid").height());
-        });
-        $("#grid").css({"color":"white"});
-        $("#grid-text").text(sum + "/25 answers correct!");
+      $("#grid").css({"background-color":"white"});
+        // var scores = sessionStorage.getItem("scores").split(",");
+        // var sum = 0;
+        // for (var i = 0; i < scores.length; i++){
+        //     sum += parseInt(scores[i]);
+        //     if (i % 5 == 0) {
+        //         $("#grid").append( "<div class='row grid-row'></div>" );
+        //     }
+        //     if (scores[i] == 1) {
+        //         $(".grid-row:last").append( "<div class='col-xs-15 grid-img'><img class='img' src='assets/img/Grid"+ (i + 1) +".png'></div>" );
+        //     } else {
+        //         $(".grid-row:last").append( "<div class='col-xs-15 grid-img incorrect'><img class='img' src='assets/img/Grid"+ (i + 1) +".png'></div>" );
+        //     }
+        // }
+        // $(".img").load(function(){
+        //     $(".incorrect").addClass("cross");
+        //     // $("#footer").height(total - header - img_text_row - $("#grid").height());
+        // });
+        // $("#grid").css({"color":"white"});
+        // $("#grid-text").text(sum + "/25 answers correct!");
   }
 
 }
